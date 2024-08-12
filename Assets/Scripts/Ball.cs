77abd4fdfedc1a9776714e6fd4f05d6747165029
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2024-08-11 18:45:49 -0700

    ball bounces but crap , paddles  go through the walls

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+    public float maxSpeed = 15f;
+    public float speedIncrease = 0.1f;
+
+    private Rigidbody2D rb;
+    private Vector2 lastVelocity;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        LaunchBall();
     }
 
-    // Update is called once per frame
+    void LaunchBall()
+    {
+        transform.position = Vector2.zero;
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(-0.5f, 0.5f);
+        Vector2 direction = new Vector2(x, y).normalized;
+        rb.velocity = direction * speed;
+    }
+
     void Update()
     {
+        lastVelocity = rb.velocity;
+    }
 
+    void FixedUpdate()
+    {
+        rb.velocity = rb.velocity.normalized * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        speed = Mathf.Min(speed + speedIncrease, maxSpeed);
+
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            float y = HitFactor(transform.position,
+                                collision.transform.position,
+                                collision.collider.bounds.size.y);
+
+            Vector2 dir = new Vector2(rb.velocity.x > 0 ? 1 : -1, y).normalized;
+            rb.velocity = dir * speed;
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            var speed = lastVelocity.magnitude;
+            var direction = Vector2.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+            rb.velocity = direction * speed;
+        }
+    }
+
+    float HitFactor(Vector2 ballPos, Vector2 paddlePos, float paddleHeight)
+    {
+        return (ballPos.y - paddlePos.y) / paddleHeight;
     }
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,18 +1,99 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 10f;
+    public bool isLeftPlayer;
+
+    private Rigidbody2D rb;
+    private PaddleControls controls;
+    private Vector2 moveInput;
+    private bool isCollidingWithWall = false;
+
+    private void Awake()
     {
-        
+        controls = new PaddleControls();
+
+        if (isLeftPlayer)
+        {
+            controls.LeftPlayer.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+            controls.LeftPlayer.Move.canceled += ctx => moveInput = Vector2.zero;
+        }
+        else
+        {
+            controls.RightPlayer.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+            controls.RightPlayer.Move.canceled += ctx => moveInput = Vector2.zero;
+        }
+
+        Debug.Log($"Paddle Awake: isLeftPlayer = {isLeftPlayer}");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        controls.Enable();
+        Debug.Log("Paddle controls enabled");
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+        Debug.Log("Paddle controls disabled");
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+        Debug.Log($"Paddle Start: Position = {rb.position}, Collision Mode = {rb.collisionDetectionMode}, Constraints = {rb.constraints}");
+    }
+
+    void FixedUpdate()
+    {
+        if (!isCollidingWithWall)
+        {
+            Vector2 movement = new Vector2(0, moveInput.y * speed * Time.fixedDeltaTime);
+            rb.velocity = new Vector2(0, movement.y); // Update velocity directly
+        }
+        else
+        {
+            rb.velocity = Vector2.zero; // Stop movement if colliding with wall
+        }
+
+        Debug.Log($"Paddle FixedUpdate: MoveInput = {moveInput}, Velocity = {rb.velocity}");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"Paddle OnCollisionEnter2D: Collided with {collision.gameObject.name}, Tag = {collision.gameObject.tag}");
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            isCollidingWithWall = true;
+            // Optionally, set vertical velocity to zero or adjust as needed
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            Debug.Log("Paddle collided with wall. Stopping movement.");
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log($"Paddle OnCollisionStay2D: Still colliding with {collision.gameObject.name}, Tag = {collision.gameObject.tag}");
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            isCollidingWithWall = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log($"Paddle OnCollisionExit2D: Stopped colliding with {collision.gameObject.name}, Tag = {collision.gameObject.tag}");
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            isCollidingWithWall = false;
+        }
     }
 }
